9955d08bbd2cdf2b27f451690210e43a8834ef9c
Josephine-Ann <josephineanngeoghegan@gmail.com>
2021-08-10 10:45:14 +0200

    drawing book and counting valleys

--- hackerrank_challenges/problem_solving/easy/drawingBook.js
+++ hackerrank_challenges/problem_solving/easy/drawingBook.js
@@ -26,35 +26,7 @@ function pageCount(lastPage, page) {
         }
     }
     return Math.min(frontCount, backCount)
-    // if book is even, start search from back 
-    // from next odd number in descending number and count 1
-    // if not, do the same and don't count 1
-    // loop through, 2 at a time:
-    // -If either current i or previous i are p, stop loop
-    // -If not, add to back count
-    // If book is 
 }
 
 console.log(pageCount(6, 2))
 
-// if (p === 1 || p === n) return 0
-// var counterFront;
-// var counterBack;
-// p % 2 !== 0 ? counterFront = 0 : counterFront = 1
-// p % 2 !== 0 ? counterBack = 0 : counterBack = 1
-// for (let i = (counterFront + 1); i < n; i += 2) {
-//     if (i === p || (i - 1) === p) {
-//         i = n
-//     } else {
-//         counterFront++
-//     }
-// }
-// var start = n - counterBack
-// for (let i = start; i > 1; i -= 2) {
-//     if (i === p || (i - 1) === p) {
-//         i = 1
-//     } else {
-//         counterBack++
-//     }
-// }
-// return Math.min(counterFront, counterBack)
