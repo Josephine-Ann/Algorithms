171eb1c162f808a136ad824ebfbe9ce1da532f2a
Josephine-Ann <josephineanngeoghegan@gmail.com>
2021-08-23 14:50:23 +0200

    day of the programmer

---
+++ hackerrank_challenges/problem_solving/easy/dayOfTheProgrammer.js
@@ -0,0 +1,16 @@
+function dayOfProgrammer(year) {
+    if (year === 1918) {
+        return "26.09.1918"
+    } else if (((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) && year > 1918) {
+        return "12.09." + year.toString()
+    } else if ((year % 4 !== 0 || year % 100 === 0) && year > 1918) {
+        return "13.09." + year.toString()
+    } else if (year % 4 === 0 && year <= 1919) {
+        return "12.09." + year.toString()
+    } else if (year % 4 !== 0 && year <= 1919) {
+        return "13.09." + year.toString()
+    }
+}
+
+// explanation
+// https://www.hackerrank.com/challenges/day-of-the-programmer/problem
