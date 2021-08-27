f6fda16e17669bb874d7ceac41700cc89c74c6c7
Josephine-Ann <josephineanngeoghegan@gmail.com>
2021-08-27 13:05:25 +0200

    append and delete

---
+++ hackerrank_challenges/problem_solving/easy/appendAndDelete.js
@@ -0,0 +1,22 @@
+function appendAndDelete(s, t, k) {
+    if (s === t) return "Yes"
+    s = s.split("")
+    var counter = 0
+    if (t.length > s.length && ((t.length - s.length) % 2) === 0) {
+        return "Yes"
+    }
+    for (let i = 0; i < s.length; i++) {
+        if (t[i] === undefined) {
+            if ((k - ((s.length) - ((s.length - t.length)))) > 1) return "Yes"
+            i = s.length
+        } else if (t[i] !== s[i]) {
+            counter += (t.length) - ((s.slice(0, i)).length) + (s.slice(i, s.length)).length
+            if ((k - counter) > 1) return "Yes"
+            i = s.length
+        }
+    }
+    return counter === k ? "Yes" : "No"
+}
+
+// explanation 
+// https://www.hackerrank.com/challenges/append-and-delete/problem
--- hackerrank_challenges/problem_solving/easy/jumpingOnClouds.js
+++ hackerrank_challenges/problem_solving/easy/jumpingOnClouds.js
@@ -1,20 +1,60 @@
-function jumpingOnClouds(c, k) {
-    var energy = 100
-    c[0] === 1 ? energy -= 3 : energy -= 1
-    for (var i = k; i < c.length; i += k) {
-        c[i] === 1 ? energy -= 3 : energy--
-        if ((i + k) > c.length) {
-            i = i + k - c.length
-            c[i] === 1 ? energy -= 3 : energy--
-        } else if ((i + k) === c.length) {
-            i === 0
-        }
+// function jumpingOnClouds(c, k) {
+//     var energy = 100
+//     c[0] === 1 ? energy -= 3 : energy -= 1
+//     for (var i = k; i < c.length; i += k) {
+//         c[i] === 1 ? energy -= 3 : energy--
+//         if ((i + k) > c.length) {
+//             i = i + k - c.length
+//             c[i] === 1 ? energy -= 3 : energy--
+//         } else if ((i + k) === c.length) {
+//             i === 0
+//         }
+//     }
+//     return energy
+// }
+
+// // console.log(jumpingOnClouds([0, 0, 1, 0, 0, 1, 1, 0], 2))
+// console.log(jumpingOnClouds([1, 1, 1, 0, 1, 1, 0, 0, 0, 0], 3))
+
+// // explanation
+// // https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited/problem
+
+function maxSubarraySum(arr, num) {
+    let maxSum = 0;
+    let tempSum = 0;
+    if (arr.length < num) return null;
+    for (let i = 0; i < num; i++) {
+        maxSum += arr[i];
     }
-    return energy
+    tempSum = maxSum;
+    for (let i = num; i < arr.length; i++) {
+        tempSum = tempSum - arr[i - num] + arr[i];
+        maxSum = Math.max(maxSum, tempSum);
+    }
+    // return maxSum;
 }
 
-// console.log(jumpingOnClouds([0, 0, 1, 0, 0, 1, 1, 0], 2))
-console.log(jumpingOnClouds([1, 1, 1, 0, 1, 1, 0, 0, 0, 0], 3))
+console.log(maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3))
 
-// explanation
-// https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited/problem
+function maxSubarraySumNaive(arr, num) {
+    var counter = 0
+    if (num > arr.length) {
+        return null;
+    }
+    var max = -Infinity;
+    for (let i = 0; i < arr.length - num + 1; i++) {
+        temp = 0;
+        for (let j = 0; j < num; j++) {
+            counter++
+            temp += arr[i + j];
+        }
+        if (temp > max) {
+            max = temp;
+        }
+    }
+    console.log(counter)
+    // return max;
+}
+
+console.log(maxSubarraySumNaive([2, 6, 9, 2, 1, 8, 5, 6, 3], 3))
+
