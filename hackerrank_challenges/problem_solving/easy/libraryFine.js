c1df77e48ebc1765fdc6b4afaef04da5109efee0
Josephine-Ann <josephineanngeoghegan@gmail.com>
2021-08-28 11:00:49 +0200

    library fine

--- hackerrank_challenges/problem_solving/easy/libraryFine.js
+++ hackerrank_challenges/problem_solving/easy/libraryFine.js
@@ -3,9 +3,9 @@ function libraryFine(d1, m1, y1, d2, m2, y2) {
     if (y1 < y2) return 0
     if (y1 > y2) return 10000
     if (m1 < m2) return 0
-    if (m1 > m2) return Math.abs((m2 - m1) * 500)
+    if (m1 > m2) return (m2 - m1) * 500
     if (d1 < d2) return 0
-    if (d1 > d2) return Math.abs((d2 - d1) * 15)
+    if (d1 > d2) return (d2 - d1) * 15
 }
 
 // explanation
