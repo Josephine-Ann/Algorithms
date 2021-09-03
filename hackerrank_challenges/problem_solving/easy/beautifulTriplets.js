1ea22c939d3156281e87d4930b4ed0813e36f58b
Josephine-Ann <josephineanngeoghegan@gmail.com>
2021-09-03 13:18:25 +0200

    beautiful triplets

---
+++ hackerrank_challenges/problem_solving/easy/beautifulTriplets.js
@@ -0,0 +1,11 @@
+function beautifulTriplets(d, arr) {
+    var finalCount = 0;
+    arr.forEach(num => {
+        if (arr.includes(d + num) && arr.includes(d + num + d)) {
+            finalCount++
+        }
+    });
+    return finalCount
+}
+
+console.log(beautifulTriplets(3, [1, 2, 4, 5, 7, 8, 10]))
