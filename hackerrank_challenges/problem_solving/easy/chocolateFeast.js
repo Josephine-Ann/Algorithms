607e0b0e25606eaef3e5d8428547fe3dece9493c
Josephine-Ann <josephineanngeoghegan@gmail.com>
2021-09-25 12:25:29 +0200

    chocolate feast

---
+++ hackerrank_challenges/problem_solving/easy/chocolateFeast.js
@@ -0,0 +1,17 @@
+function chocolateFeast(n, c, m) {
+    var brsPrchsed = Math.floor(n / c)
+    var wrps = brsPrchsed
+    var buying;
+    var wrpsRtrd = 0;
+    var wrpsLft = 0;
+    while (wrps >= m) {
+        buying = Math.floor(wrps / m)
+        wrpsLft = (wrps % m)
+        wrpsRtrd = (buying)
+        brsPrchsed += (buying)
+        wrps = (wrpsRtrd + wrpsLft)
+    }
+    return brsPrchsed
+}
+
+console.log(chocolateFeast(7, 3, 2))
