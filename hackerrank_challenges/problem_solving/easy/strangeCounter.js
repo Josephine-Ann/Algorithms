4db389a3a9af7806039453d499f02d7490e87bc2
Josephine-Ann <josephineanngeoghegan@gmail.com>
2021-09-27 12:06:47 +0200

    strange counter

--- hackerrank_challenges/problem_solving/easy/strangeCounter.js
+++ hackerrank_challenges/problem_solving/easy/strangeCounter.js
@@ -2,7 +2,7 @@ function strangeCounter(t) {
     if (t < 3) return 4 - t
     var first = Math.ceil(Math.log2((t + 2) / 3))
     if ((((2 ** first) * 3)) - 2 === t) return (((2 ** first) * 3)) - 2
-    return (((2 ** first) * 3) < t) ? ((2 ** first) * 3) - 2 + (((2 ** first) * 3) - t) : ((2 ** first) * 3) - 2 - t // -> 46/time of SP of 45, prev
+    return (((2 ** first) * 3) < t) ? ((2 ** first) * 3) - 2 + (((2 ** first) * 3) - t) : ((2 ** first) * 3) - 2 - t
 }
 
 // explanation
