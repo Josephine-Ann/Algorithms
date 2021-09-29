8952b07ee711f097ea9f73ec385ceef49ce42da6
Josephine-Ann <josephineanngeoghegan@gmail.com>
2021-09-29 12:04:10 +0200

    string construction

--- hackerrank_challenges/problem_solving/easy/gameOfThrones.js
+++ hackerrank_challenges/problem_solving/easy/gameOfThrones.js
@@ -16,10 +16,8 @@ function gameOfThrones(s) {
         } else if ((freqC[char] % 2 !== 0) && (sLength % 2 === 0) && counterOdd === 2 && (freqC[char] !== 1)) {
             return "NO"
         } else if ((freqC[char] % 2 !== 0) && (sLength % 2 === 0) && (freqC[char] === 1) && (counterSingle < 1)) {
-            console.log(char + " yo")
             counterSingle++
         } else if ((freqC[char] % 2 !== 0) && (sLength % 2 === 0) && (freqC[char] === 1) && (counterSingle >= 1)) {
-            console.log(char + " yi")
             return "NO"
         }
     }
