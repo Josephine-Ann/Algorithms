1c3f663c0e95063a88c011dadea6f0e1b399c45b
Josephine-Ann <josephineanngeoghegan@gmail.com>
2021-09-30 13:22:54 +0200

    jumping on clouds

--- hackerrank_challenges/problem_solving/easy/jumpingOnClouds.js
+++ hackerrank_challenges/problem_solving/easy/jumpingOnClouds.js
@@ -1,60 +1,12 @@
-// function jumpingOnClouds(c, k) {
-//     var energy = 100
-//     c[0] === 1 ? energy -= 3 : energy -= 1
-//     for (var i = k; i < c.length; i += k) {
-//         c[i] === 1 ? energy -= 3 : energy--
-//         if ((i + k) > c.length) {
-//             i = i + k - c.length
-//             c[i] === 1 ? energy -= 3 : energy--
-//         } else if ((i + k) === c.length) {
-//             i === 0
-//         }
-//     }
-//     return energy
-// }
-
-// // console.log(jumpingOnClouds([0, 0, 1, 0, 0, 1, 1, 0], 2))
-// console.log(jumpingOnClouds([1, 1, 1, 0, 1, 1, 0, 0, 0, 0], 3))
-
-// // explanation
-// // https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited/problem
-
-function maxSubarraySum(arr, num) {
-    let maxSum = 0;
-    let tempSum = 0;
-    if (arr.length < num) return null;
-    for (let i = 0; i < num; i++) {
-        maxSum += arr[i];
+function jumpingOnClouds(c) {
+    var i = 0
+    var moves = 0
+    while (i < (c.length - 1)) {
+        moves++
+        c[i + 2] !== 1 ? i += 2 : i++
     }
-    tempSum = maxSum;
-    for (let i = num; i < arr.length; i++) {
-        tempSum = tempSum - arr[i - num] + arr[i];
-        maxSum = Math.max(maxSum, tempSum);
-    }
-    // return maxSum;
+    return moves
 }
 
-console.log(maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3))
-
-function maxSubarraySumNaive(arr, num) {
-    var counter = 0
-    if (num > arr.length) {
-        return null;
-    }
-    var max = -Infinity;
-    for (let i = 0; i < arr.length - num + 1; i++) {
-        temp = 0;
-        for (let j = 0; j < num; j++) {
-            counter++
-            temp += arr[i + j];
-        }
-        if (temp > max) {
-            max = temp;
-        }
-    }
-    console.log(counter)
-    // return max;
-}
-
-console.log(maxSubarraySumNaive([2, 6, 9, 2, 1, 8, 5, 6, 3], 3))
-
+// explanation
+// https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem
