9fa6a2e55a0f28a29ca8c1b4bb64a65323ef8844
josephine geoghegan <josephineanngeoghegan@gmail.com>
2022-07-14 12:59:50 +0200

    closest numbers

---
+++ hackerrank_challenges/interviewPrep/easy/closestNumbers.js
@@ -0,0 +1,14 @@
+function closestNumbers(arr) {
+    arr.sort((a,b)=>a-b);
+    const finalSets = [];
+    let min;
+    for (let i = 0; i < (arr.length - 1); i++) {
+           if (!min || min > (Math.abs(arr[i] - arr[i + 1]))) min =  Math.abs(arr[i] - arr[i + 1])
+    }
+    for (let i = 0; i < (arr.length - 1); i++) {
+        if (Math.abs(arr[i] - arr[i + 1]) === min) finalSets.push(arr[i], arr[i+1])
+    }
+    return finalSets
+}
+const items = [5, 4, 3, 2]
+console.log(closestNumbers(items))
